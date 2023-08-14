0ef3706b70b5d34558f7cdf0cac66090e26f6678
JamieYau <jamie.yau2002@me.com>
2023-08-14 16:48:57 +0100

    Add clicking away from dropdown causes it to hide

--- Dropdown/script.js
+++ Dropdown/script.js
@@ -5,3 +5,12 @@ dropdownButton.addEventListener("click", () => {
   // Toggle the "show" class to control visibility
   dropdownMenu.classList.toggle("show");
 });
+
+// Add a click event listener to the document
+document.addEventListener("click", (event) => {
+  // Check if the clicked element is not the dropdown button or menu
+  if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    // Hide the dropdown menu
+    dropdownMenu.classList.remove("show");
+  }
+});
